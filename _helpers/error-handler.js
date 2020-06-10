5d59c9f77044e330255c228140108d30413d60bc
bc-ubuntu-desktop <jblaise0912@gmail.com>
2020-06-09 21:50:21 -0700

    Updated json printout

--- _helpers/error-handler.js
+++ _helpers/error-handler.js
@@ -8,8 +8,8 @@ function errorHandler(err, req, res, next) {
 
     if (err.name === 'UnauthorizedError') {
         // jwt authentication error
-        // return res.status(401).json({ message: 'Invalid Token' });
-        return res.status(401).json('Unauthorized Access');
+        return res.status(401).json({ message: 'Invalid Token' });
+        // return res.status(401).json('Unauthorized Access');
     }
 
     // default to 500 server error
