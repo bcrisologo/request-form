db898ca4d37c880c18e398d46cc7e07ad0640788
Blaise <jblaise0912@gmail.com>
2020-06-11 14:13:52 -0700

    Updated model

--- models/RequestForm.js
+++ models/RequestForm.js
@@ -4,11 +4,34 @@
 var mongoose = require('mongoose');
 
 var RequestFormSchema = new mongoose.Schema({
-	first_name: String,
-	last_name: String,
-	phone_number: String,
-	organization: String,
-	request: String,
+	first_name: {
+		type: String,
+		min: [2, 'Too few letters'],
+		max: 15,
+		required: [true, 'First Name required']
+	},
+	last_name: {
+		type: String,
+		min: [2, 'Too few letters'],
+		max: 15,
+		required: [true, 'Last Name required']
+	},
+	phone_number: {
+		type: String,
+		min: [10, 'Too few letters'],
+		max: 15,
+		required: [true, 'Phone number required']
+	},
+	organization: {
+		type: String,
+		min: [3, 'Too few letters'],
+		max: 20,
+		required: [true, 'Organization required']
+	},
+	request: {
+		type: String,
+		required: [true, 'Your request is required']
+	},
 	updated_at: {
 		type: Date,
 		default: Date.now
