1833f24bef7166cf2e800ea5c417cbb0dbeda953
Blaise <jblaise0912@gmail.com>
2020-06-11 15:28:25 -0700

    Added login page

--- models/RequestForm.js
+++ models/RequestForm.js
@@ -4,34 +4,11 @@
 var mongoose = require('mongoose');
 
 var RequestFormSchema = new mongoose.Schema({
-	first_name: {
-		type: String,
-		min: [2, 'Too few letters'],
-		max: 15,
-		required: [true, 'First Name required']
-	},
-	last_name: {
-		type: String,
-		min: [2, 'Too few letters'],
-		max: 15,
-		required: [true, 'Last Name required']
-	},
-	phone_number: {
-		type: String,
-		min: [10, 'Too few letters'],
-		max: 15,
-		required: [true, 'Phone number required']
-	},
-	organization: {
-		type: String,
-		min: [3, 'Too few letters'],
-		max: 20,
-		required: [true, 'Organization required']
-	},
-	request: {
-		type: String,
-		required: [true, 'Your request is required']
-	},
+	first_name: String,
+	last_name: String,
+	phone_number: String,
+	organization: String,
+	request: String,
 	updated_at: {
 		type: Date,
 		default: Date.now
@@ -41,5 +18,4 @@ var RequestFormSchema = new mongoose.Schema({
 // Mongoose index section for querying names and organization
 RequestFormSchema.index({ first_name: "text", last_name: "text", organization: "text" });
 
-// Named the collection "listofforms"
-module.exports = mongoose.model('RequestForm', RequestFormSchema, 'listofforms');
+module.exports = mongoose.model('RequestForm', RequestFormSchema);
