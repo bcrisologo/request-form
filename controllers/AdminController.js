4e034354b5f2dd33624785b0d3d8d26c875b9630
bc-ubuntu-desktop <jblaise0912@gmail.com>
2020-06-15 12:29:54 -0700

    Updated controller

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -7,3 +7,33 @@ var AdminModel = require("../models/AdminModel.js");		// Schema for AdminModel
 var adminController = {};
 
 // CRUD functions =====================================================
+
+// EDIT admin page render =============================
+adminController.edit = function(req, res) {
+	AdminModel.findOne({ _id: req.params.id }).exec(function(err, user) {
+		if(err) {
+			console.log("Error: ", err)
+		}
+		else {
+			res.render("../views/forms/adminsettings", { user: user });
+		}
+	});
+};
+
+// UPDATE admin username and password =============================
+adminController.update = function(req, res) {
+	AdminModel.findByIdAndUpdate(req.params.id, {
+		$set: {
+			username: req.body.username,
+			password: req.body.password
+		}
+	}, function(err, user) {
+		if(err) {
+			console.log("Error: ", err);
+			res.render(("../views/forms/adminsettings", { user: user });
+		}
+		else {
+			res.redirect("/forms");
+		}
+	});
+};
