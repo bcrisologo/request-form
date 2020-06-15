68f77a005f10cd6aed8abfeea1fdac1bfb50288e
bc-ubuntu-desktop <jblaise0912@gmail.com>
2020-06-15 16:21:53 -0700

    Changing hash and salt progress

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -10,7 +10,7 @@ var adminController = {};
 
 // EDIT admin page render =============================
 adminController.edit = function(req, res) {
-	AdminModel.findOne({ _id: req.params.id }).exec(function(err, adminuser) {
+	AdminModel.findOne({ username: 'admin' }).exec(function(err, adminuser) {
 		if(err) {
 			console.log("Error: ", err)
 		}
@@ -22,21 +22,31 @@ adminController.edit = function(req, res) {
 
 // UPDATE admin username and password =============================
 adminController.update = function(req, res) {
-	AdminModel.findOneAndUpdate({ username: 'admin'}, {
-		// NEED TO CHANGE THIS TO UPDATE SALT AND HASH FOR PASSWORD
-		// ALL THIS DOES IS INSERT NEW PASSOWRD
-		$set: {
-			password: req.body.password
+
+	AdminModel.findOneAndUpdate({ username: 'admin' })
+	.then(function(adminuser) {
+		if(adminuser) {
+			
 		}
-	}, function(err, adminuser) {
-		if(err) {
-			console.log("Error: ", err);
-			res.render("../views/forms/adminsettings", { adminuser: adminuser });
+	})
+
+	/*  // THIS WORKS!!! But need verification of current password before changing it
+	AdminModel.findOneAndUpdate({ username: 'admin' })
+	.then(function(adminuser) {
+		if(adminuser) {
+			adminuser.setPassword(req.body.password, function() {
+				adminuser.save();
+				console.log("Successful password change!");
+				res.render("../views/forms");
+			});
 		}
 		else {
-			res.redirect("/forms");
+			res.status(500).json({message: 'This user does not exist'});
 		}
-	});
+	}, function(err) {
+		console.error(err);
+	})
+	*/
 };
 
 module.exports = adminController;
