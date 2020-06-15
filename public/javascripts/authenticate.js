33d3ed91c119b1ee120473275faa7db560a3e413
bc-ubuntu-desktop <jblaise0912@gmail.com>
2020-06-14 18:42:49 -0700

    Updated authenticate.js

--- public/javascripts/authenticate.js
+++ public/javascripts/authenticate.js
@@ -3,7 +3,7 @@
 // For login page error checks ===================================
 
 const urlParams = new URLSearchParams(window.location.search);
-// const info = urlParams.get('info');			// For full info on input
+const info = urlParams.get('info');			// For full info on input
 
 if(info) {
 	const errorMessage = document.getElementById("error-message");
