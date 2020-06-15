1e489af9020189331bfc874be08282ab9cb81496
bc-ubuntu-desktop <jblaise0912@gmail.com>
2020-06-14 18:06:03 -0700

    Updated comments

--- public/javascripts/authenticate.js
+++ public/javascripts/authenticate.js
@@ -3,11 +3,11 @@
 // For login page error checks ===================================
 
 const urlParams = new URLSearchParams(window.location.search);
-const info = urlParams.get('info');
+// const info = urlParams.get('info');			// For full info on input
 
 if(info) {
 	const errorMessage = document.getElementById("error-message");
-	// errorMessage.innerText = info;
+	// errorMessage.innerText = info;			// Error display looks too messy
 	errorMessage.innerText = "Incorrect Username or Password";
 	errorMessage.style.display = "block";
 }
